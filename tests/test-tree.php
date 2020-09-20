1fd3595bd2a5f696363af7f30e30a00619c1398b
Max Lyuchin <maxim.lyuchin@gmail.com>
2020-09-20 19:08:36 +0300

    Test for the large tree server-side rendering.
    Assumes that large tree from the CLI example (9 levels, 3 teams) should be generated in less than 5 seconds.

--- tests/test-tree.php
+++ tests/test-tree.php
@@ -30,4 +30,36 @@ class Rusty_Inc_Org_Chart_Tree_Test extends WP_UnitTestCase {
 	    ] ];
 		$this->assertEquals( $expected, ( new Rusty_Inc_Org_Chart_Tree( $list_of_teams ) )->get_nested_tree() );
 	}
+
+	/**
+	 * Creates large tree (3 teams / 9 levels from the CLI example)
+	 * and checks if it's converted to nested in less than 5 seconds
+	 * (should be enough for most test environments)
+	 */
+	public function test_really_big_tree() {
+		$levels = 9;
+		$sub_teams = 3;
+		$flat_tree = [ 1 => [ 'id' => 1, 'parent_id' => null, 'emoji' => '🍓', 'name' => 'Land of Nice and Competent People' ] ];
+		$next_id = 2;
+		$previous_level = [ 1 ];
+		for( $level = 0; $level < $levels - 1; $level++ ) {
+			$this_level = [];
+			foreach( $previous_level as $parent_id ) {
+				for ( $sub_team = 0; $sub_team < $sub_teams; $sub_team++ ) {
+					$id = $next_id++;
+					$flat_tree[$id] = [ 'id' => $id, 'parent_id' => $parent_id, 'emoji' => '👋', 'name' => "Respected Group 🍉 ($id)" ];
+					$this_level[] = $id;
+				}
+			}
+			$previous_level = $this_level;
+		}
+
+		$start_time = microtime( true );
+		$tree = ( new Rusty_Inc_Org_Chart_Tree( $flat_tree ) )->get_nested_tree();
+		$end_time = microtime( true );
+
+		$less_than_five_seconds = ( $end_time - $start_time ) < 5;
+		$this->assertTrue( $less_than_five_seconds );
+	}
+
 }
